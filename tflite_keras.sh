7a197121944f8b30a90b3c8e8036f4b293720616
Sachin Sundar <83456900+Sachin-2007@users.noreply.github.com>
2022-09-21 21:50:03 +0530

    Update tflite_keras.sh

--- tflite_keras.sh
+++ tflite_keras.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 python3 -m pip install tflite-runtime
 sudo pip3 install keras_applications==1.0.8 --no-deps
