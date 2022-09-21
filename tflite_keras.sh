a41e70254e04594dff0ae8234a64acbe50e8c64f
Sachin Sundar <83456900+Sachin-2007@users.noreply.github.com>
2022-09-21 21:43:04 +0530

    Create tflite_keras.sh

---
+++ tflite_keras.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+python3 -m pip install tflite-runtime
+sudo pip3 install keras_applications==1.0.8 --no-deps
+sudo pip3 install keras_preprocessing==1.1.0 --no-deps
